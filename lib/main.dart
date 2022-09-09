fbc541c567565e1ca66c9556e549de956f1ba0bf
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-09-09 14:51:25 +0530

    Completed Basic Designing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final TextEditingController _number = TextEditingController();
+String _num = "";
+
+// final Uri _url = ;
 
 void main() {
   runApp(const WhatsappSender());
@@ -16,8 +22,61 @@ class _WhatsappSenderState extends State<WhatsappSender> {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(),
+        home: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Center(child: Text('Whatsapp Sender')),
+              backgroundColor: Colors.teal[800],
+            ),
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  TextField(
+                    controller: _number,
+                    cursorColor: Colors.red,
+                    style: const TextStyle(
+                        color: Colors.green,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      // labelText: 'hey',
+
+                      prefixIcon: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      focusedBorder: InputBorder.none,
+                      hintText: "Enter number with Country Code",
+                      hintStyle: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 25,
+                      ),
+                    ),
+                    onChanged: (text) {
+                      _num = text;
+                    },
+                  ),
+                  MaterialButton(
+                    color: Colors.deepOrange,
+                    ,
+                    height: 60,
+                    minWidth: 150,
+                    onPressed: () async {
+                      if (!await launchUrl(Uri.parse('https://wa.me/$_num'),
+                          mode: LaunchMode.externalApplication)) {
+                        throw 'Could not launch';
+                      }
+                    },
+                    child: const Text('Send Message'),
+                  ),
+                  const SizedBox(height: 10),
+                ]),
+          ),
         ));
   }
 }
