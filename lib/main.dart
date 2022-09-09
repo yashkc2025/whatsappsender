d077689ec25e37a7bc3fe6fae14e57931e3eceaa
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-09-09 14:57:15 +0530

    Added background image

--- lib/main.dart
+++ lib/main.dart
@@ -28,54 +28,61 @@ class _WhatsappSenderState extends State<WhatsappSender> {
               title: const Center(child: Text('Whatsapp Sender')),
               backgroundColor: Colors.teal[800],
             ),
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  TextField(
-                    controller: _number,
-                    cursorColor: Colors.red,
-                    style: const TextStyle(
-                        color: Colors.green,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      // labelText: 'hey',
-
-                      prefixIcon: Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      focusedBorder: InputBorder.none,
-                      hintText: "Enter number with Country Code",
-                      hintStyle: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 25,
-                      ),
+            body: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://i.pinimg.com/736x/85/04/30/850430a750fb80c1ebaa5e740fc7cbd6.jpg'),
+                    fit: BoxFit.cover),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      height: 40,
                     ),
-                    onChanged: (text) {
-                      _num = text;
-                    },
-                  ),
-                  MaterialButton(
-                    color: Colors.deepOrange,
-                    ,
-                    height: 60,
-                    minWidth: 150,
-                    onPressed: () async {
-                      if (!await launchUrl(Uri.parse('https://wa.me/$_num'),
-                          mode: LaunchMode.externalApplication)) {
-                        throw 'Could not launch';
-                      }
-                    },
-                    child: const Text('Send Message'),
-                  ),
-                  const SizedBox(height: 10),
-                ]),
+                    TextField(
+                      controller: _number,
+                      cursorColor: Colors.red,
+                      style: const TextStyle(
+                          color: Colors.green,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        // labelText: 'hey',
+
+                        prefixIcon: Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        focusedBorder: InputBorder.none,
+                        hintText: "Enter number with Country Code",
+                        hintStyle: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 25,
+                        ),
+                      ),
+                      onChanged: (text) {
+                        _num = text;
+                      },
+                    ),
+                    MaterialButton(
+                      color: Colors.deepOrange,
+                      height: 60,
+                      minWidth: 150,
+                      onPressed: () async {
+                        if (!await launchUrl(Uri.parse('https://wa.me/$_num'),
+                            mode: LaunchMode.externalApplication)) {
+                          throw 'Could not launch';
+                        }
+                      },
+                      child: const Text('Send Message'),
+                    ),
+                    const SizedBox(height: 10),
+                  ]),
+            ),
           ),
         ));
   }
